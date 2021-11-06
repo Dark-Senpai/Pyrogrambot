7f9536c761562dfe8ebc308fd87aabeda81883c9
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-11-06 18:51:28 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from pyrogram import filters 
+  from Config import *
+  
+  @app.on_message(filters.command(["start"]))
+  async def start(bot, message):
+    await bot.send_message(
+      message.chat.id,
+      text="**Hi! I am online. Join my channel**!",
+      reply_markup=InlineKeyboardMarkup(
+        [InlineKeyboardButton("CHANNEL", url="https://t.me/Animes_Encoded")],
+      ),
+    )
