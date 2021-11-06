9c9692ed19d504b9490c49215d1d599e9e06fb15
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-11-06 18:56:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-
+from pyrogram import filters 
+from Config import *
+  
+@app.on_message(filters.command(["start"]))
+async def start(bot, message):
+  await bot.send_message(
+    message.chat.id,
+    text="**Hi! I am online. Click the below button to know my master**!",
+    reply_markup=InlineKeyboardMarkup(
+      [InlineKeyboardButton('BOSS', url="https://t.me/Parth_Senpai")],
+    ),
+  )
